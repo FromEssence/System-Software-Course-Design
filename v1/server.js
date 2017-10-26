091b8146c1b3c788cf80fb5d27416e84d9d42980
Little Prince <tjw199723@163.com>
2017-10-26 21:40:00 +0800

    Update server.js

--- v1/server.js
+++ v1/server.js
@@ -1,29 +1,3 @@
 var express = require('express');
 var http = require('http');
 
-//var favicon = require('static-favicon');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var cons = require('consolidate');
-var route = require("./route");
-var app = express();
-
-
-
-app.engine('html', cons.swig);
-app.set('view engine', 'html');
-//app.use(favicon());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extend:true
-}));
-app.use(cookieParser());
-
-// initialize application route config.
-route.init(app);
-http.createServer(app).listen(8888, function() {
-   console.log("node server start!!!")
-});
-
-
-module.exports = app;
