69a81b64dacd117dcefa3e9e75c51cc88940b148
Little Prince <tjw199723@163.com>
2017-10-30 09:25:12 +0800

    Create routers.js

---
+++ routers.js
@@ -0,0 +1,51 @@
+///Routers
+router.post('/address', function(req, res){
+    console.log("post address receieved");
+    var data = req.body.userid;
+    var sql = 'select '
+});
+
+router.post('/information', function(req, res){
+   console.log("post information receieved"); 
+});
+
+router.post('/photonumber', function(req, res){
+   console.log("post photonumber receieved"); 
+});
+
+router.post('/security', function(req, res){
+   console.log("post security receieved"); 
+});
+
+router.post('/login', function(req, res){
+   console.log("post login receieved"); 
+});
+
+router.post('/register', function(req, res){
+   console.log("post register receieved"); 
+});
+
+/// ?
+router.post('/message', function(req, res){
+   console.log("post message receieved"); 
+});
+///
+router.post('/prolist', function(req, res){
+   console.log("post prolist receieved"); 
+});
+
+router.post('/trolley', function(req, res){
+   console.log("post trolley receieved"); 
+});
+
+router.post('/order', function(req, res){
+   console.log("post order receieved"); 
+});
+
+router.post('/search', function(req, res){
+   console.log("post search receieved"); 
+});
+
+router.post('/single', function(req, res){
+   console.log("post single receieved"); 
+});
